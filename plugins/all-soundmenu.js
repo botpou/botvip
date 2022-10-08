46a87c410da10597f06c7e9582fa40403cc6d8c6
andymrlit <andysebastien14@gmail.com>
2022-10-07 22:16:33 -0500

    Update all-soundmenu.js

--- plugins/all-soundmenu.js
+++ plugins/all-soundmenu.js
@@ -190,7 +190,7 @@ const sections = [
 ]
 
 const listMessage = {
-  text: `Script : https://youtube.com/channel/UCjoPsysjCn2Qa0dRalUb2mg`,
+  text: `Script : https://instagram.com/andy_mr_lit`,
   footer: teks,
   title: 'ᴄʟɪᴄᴋ ʙᴜᴛᴛᴏɴ ɴᴏᴡ!',
   buttonText: "Pʟᴀʏɪɴɢ Nᴏᴡ...",
